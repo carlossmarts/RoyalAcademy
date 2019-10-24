af56e29028e1a0cba799f2cb43481a4e6ac32d37
MVelasquez98 <43156596+MVelasquez98@users.noreply.github.com>
2019-10-24 12:34:28 -0300

    Sede BL y DAO

---
+++ src/test/TestSede.java
@@ -0,0 +1,17 @@
+package test;
+import negocio.SedeBL;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SedeBL bl =new SedeBL();
+		try {
+		bl.agregarSede(1, 1, 1, 1, "1842", "Barbier", 2746);
+		System.out.println(bl.traerSede("Barbier", 2746).toString());
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
